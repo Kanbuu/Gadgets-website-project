2473e8f8b804748a5605eeb7384adec941fe7ce1
Kanbuu <143351230+Kanbuu@users.noreply.github.com>
2026-07-14 00:33:00 +0000

    completing the project

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,34 @@
-const hamburgerButton = document.querySelector(".hamburger-icon-js");
-const closeHamburgerButton = document.querySelector(".close-hamburger");
+// const hamburgerButton = document.querySelector(".hamburger-icon-js");
+// const closeHamburgerButton = document.querySelector(".close-hamburger");
 
-const hamburgerSectionEl = document.querySelector(".hamburger-section");
+// const hamburgerSectionEl = document.querySelector(".hamburger-section");
 
-const hamburgerMainCont = document.querySelector(".h-main-container");
+// const hamburgerMainCont = document.querySelector(".h-main-container");
 
-hamburgerButton.addEventListener('click',()=>{
-  hamburgerSectionEl.classList.add('active');
-  hamburgerMainCont.classList.add('vate');
+// hamburgerButton.addEventListener('click',()=>{
+//   hamburgerSectionEl.classList.add('active');
+//   hamburgerMainCont.classList.add('vate');
+// });
+
+// closeHamburgerButton.addEventListener('click',()=> {
+//   hamburgerSectionEl.classList.remove('active');
+
+// })
+
+
+const hamburger = document.querySelector(".hamburger-icon-js");
+const mobileMenu = document.querySelector(".mobile-menu-js");
+const closeBtn = document.querySelector(".close-menu-js");
+const overlay = document.querySelector(".menu-overlay-js");
+
+hamburger.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
 });
 
-closeHamburgerButton.addEventListener('click',()=> {
-  hamburgerSectionEl.classList.remove('active');
+closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+});
 
-})
+overlay.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+});
